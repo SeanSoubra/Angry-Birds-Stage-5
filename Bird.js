5f402eb1e5cb9e81c711d993c0d6c2dd88cfa7e7
SeanSoubra <72533785+SeanSoubra@users.noreply.github.com>
2021-01-06 22:01:10 -0500

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,13 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    Matter.Body.setDensity(this.body, 2.0) 
+    this.image = loadImage("sprites/bird.png");
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+    super.display();
+  }
+}
